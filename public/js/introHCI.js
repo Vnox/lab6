cec8718db5b1445363fe5ac50c18557ee8f54985
Shuangli Zhou <shz103@ucsd.edu>
2017-02-16 13:23:05 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,18 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	var projectHTML = 
+    '<img src="' + result['image'] + '" class="img detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] + '</small></p>' +
+    '<p><small>' + result['summary'] + '</small></p>';
+
+    $(".details").html(projectHTML);
 }
 
 /*
@@ -34,5 +45,19 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	//res.redirect('/palette')
+	$.get('/palette', fthiss);
+	
+}
+
+
+function fthiss(colorObject){
+	var colors = colorObject['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 	console.log("User clicked on color button");
+
 }
